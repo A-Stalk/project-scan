dc628d545d59e7fe424c32130380845a49ce607d
Andrey Gorchakov <agorchakov@yandex.ru>
2023-05-30 17:02:51 +0300

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/project-scan',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
